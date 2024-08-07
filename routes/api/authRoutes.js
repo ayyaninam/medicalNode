f25dea9c7be89ddee5e5ab1034b59bf0701e0920
Ayyan Inam <ayaaninam555@gmail.com>
2024-08-07 19:48:25 +0500

    First Commit

--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const authController = require('../../controllers/authController');
 
 router.post('/home', authController.home);
-router.post('/register', authController.register);
-router.post('/login', authController.login);
+router.post('/patient-register', authController.register);
+router.post('/patient-login', authController.login);
 router.post('/access_code_verification_patient/:uid', authController.accesscodeverfication);
 router.post('/forgot_password_patient', authController.forgetpassword);
 
